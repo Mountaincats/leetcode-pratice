3d1a4e679435ffdbef92a2023ea95aa1ab87f87d
Liang Senmao <3869271449@qq.com>
2025-12-19 11:54:08 +0800

    optimize ex202 method1

--- ex202/method1.c
+++ ex202/method1.c
@@ -27,26 +27,57 @@ void insert(int ikey, int ival) {
     }
 }
 
+// next() 是纯函数，更容易被内联和优化
+int next(int n) {
+    int val = 0;
+    while (n) {
+        int num = n % 10;
+        val += num * num;
+        n /= 10;
+    }
+
+    return val;
+}
+
 bool isHappy(int n) {
     hashtable = NULL;
 
-    int temp = n;
     int key = n;
     while (true) {
-        int val = 0;
-        while (temp) {
-            int num = temp % 10;
-            val += num * num;
-            temp /= 10;
-        }
+        int val = next(key);
 
         if (val == 1) return true; 
 
         struct hashTable* it = find(val);
         if (it == NULL) {
             insert(key, val);
-            key = temp = val;
+            key = val;
         }
         else return false;
     }
 }
+
+// bool isHappy(int n) {
+//     hashtable = NULL;
+
+//     int temp = n;
+//     // 跨循环状态变量 temp，优化空间更小
+//     int key = n;
+//     while (true) {
+//         int val = 0;
+//         while (temp) {
+//             int num = temp % 10;
+//             val += num * num;
+//             temp /= 10;
+//         }
+
+//         if (val == 1) return true; 
+
+//         struct hashTable* it = find(val);
+//         if (it == NULL) {
+//             insert(key, val);
+//             key = temp = val;
+//         }
+//         else return false;
+//     }
+// }
