87ff7bb27e3c3934ec9e1d0ce67b7b360350eab1
Liang Senmao <3869271449@qq.com>
2026-06-06 14:15:56 +0800

    solve: complete ex1047

---
+++ exercise/Stack/ex1047/method1.c
@@ -0,0 +1,15 @@
+char* removeDuplicates(char* s) {
+    char* stack = malloc(sizeof(char) * strlen(s));
+    int top = -1;
+    while (*s != '\0') {
+        if (top == -1) stack[++top] = *s;
+        else {
+            if (stack[top] == *s) top--;
+            else stack[++top] = *s;
+        }
+        s++;
+    }
+    stack[++top] = '\0';
+
+    return stack;
+}
