8c24fe46391b5a9da0779c572894b32143b4b373
Liang Senmao <3869271449@qq.com>
2026-06-18 07:16:18 +0800

    solve: complete ex35

---
+++ exercise/BinarySearch/ex35/method1.c
@@ -0,0 +1,11 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0, right = numsSize - 1, mid;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (nums[mid] < target) left = mid + 1;
+        else if (nums[mid] > target) right = mid - 1;
+        else return mid;
+    }
+
+    return left;
+}
