5fe8cc260a5eda52891eebb97e2751d265b33af4
Liang Senmao <3869271449@qq.com>
2026-06-26 23:25:15 +0800

    solve: complete ex162

---
+++ exercise/BinarySearch/ex162/method1.c
@@ -0,0 +1,7 @@
+int findPeakElement(int* nums, int numsSize) {
+    int i = 0;
+    for (i = 0; i < numsSize - 1; i++) {
+        if (nums[i] > nums[i + 1]) break;
+    }
+    return i;
+}
---
+++ exercise/BinarySearch/ex162/method2.c
@@ -0,0 +1,11 @@
+int findPeakElement(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] < nums[mid + 1]) left = mid + 1;
+        else right = mid;
+    }
+
+    return left;
+}
