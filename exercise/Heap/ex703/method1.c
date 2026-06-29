6b6cf0f9a9ecb1af8fa5be7c6a10c5bc1ad33bb4
Liang Senmao <3869271449@qq.com>
2026-06-29 09:55:03 +0800

    solve: complete ex703

---
+++ exercise/Heap/ex703/method1.c
@@ -0,0 +1,90 @@
+struct Heap {
+    int* heap;
+    int heapSize;
+    bool (*cmp)(int, int);
+};
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+
+void swap(int* a, int* b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void init(struct Heap* obj, int n, bool (*cmp)(int, int)) {
+    // 索引从 1 开始
+    obj->heap = malloc(sizeof(int) * (n + 1));
+    obj->heapSize = 0;
+    obj->cmp = cmp;
+}
+
+void push(struct Heap* obj, int val) {
+    int child = ++(obj->heapSize), father = child >> 1;
+    obj->heap[child] = val;
+    while (father) {
+        if (obj->cmp(obj->heap[child], obj->heap[father])) {
+            break;
+        }
+        swap(&(obj->heap[child]), &(obj->heap[father]));
+        child = father;
+        father = child >> 1;
+    }
+}
+
+void pop(struct Heap* obj) {
+    obj->heap[1] = obj->heap[(obj->heapSize)--];
+    int father = 1, child = father << 1;
+    while (child <= obj->heapSize) {
+        if (child + 1 <= obj->heapSize && obj->cmp(obj->heap[child], obj->heap[child + 1])) child++;
+        if (obj->cmp(obj->heap[child], obj->heap[father])) break;
+        swap(&(obj->heap[child]), &(obj->heap[father]));
+        father = child;
+        child = father << 1;
+    }
+}
+
+int top(struct Heap* obj) {
+    return obj->heap[1];
+}
+
+typedef struct {
+    struct Heap* heap;
+    int maxSize;
+} KthLargest;
+
+int kthLargestAdd(KthLargest* obj, int val) {
+    push(obj->heap, val);
+    if (obj->heap->heapSize > obj->maxSize) {
+        pop(obj->heap);
+    }
+    return top(obj->heap);
+}
+
+KthLargest* kthLargestCreate(int k, int* nums, int numsSize) {
+    KthLargest* ret = malloc(sizeof(KthLargest));
+    ret->heap = malloc(sizeof(struct Heap));
+    init(ret->heap, k + 1, cmp);
+    ret->maxSize = k;
+    for (int i = 0; i < numsSize; i++) {
+        kthLargestAdd(ret, nums[i]);
+    }
+
+    return ret;
+}
+
+void kthLargestFree(KthLargest* obj) {
+    free(obj->heap->heap);
+    free(obj->heap);
+    free(obj);
+}
+
+/**
+ * Your KthLargest struct will be instantiated and called as such:
+ * KthLargest* obj = kthLargestCreate(k, nums, numsSize);
+ * int param_1 = kthLargestAdd(obj, val);
+ 
+ * kthLargestFree(obj);
+*/
