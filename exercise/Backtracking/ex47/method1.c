30353e899b26523dd74d45ba73f1ed78777b2e6b
Liang Senmao <3869271449@qq.com>
2026-08-04 21:08:15 +0800

    solve: complete ex47

--- exercise/Backtracking/ex47/method1.c
+++ exercise/Backtracking/ex47/method1.c
@@ -3,40 +3,45 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int** permute(int *nums, int numsSize, int *returnSize, int **returnColumnSizes) {
+
+// method1 需要知道数据范围来确定哈希表大小，能处理的数据范围有限的同时还会因数据大小影响使用的存储空间大小
+
+int** permuteUnique(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
     *returnSize = 0;
 
     int ansSize = 1;
-    for (int i = 2; i <= numsSize; i++) {
+    for (int i = 1; i <= numsSize; i++) {
         ansSize *= i;
     }
-    int **ans = malloc(sizeof(int *) * ansSize);
-    int *colans = malloc(sizeof(int) * ansSize);
 
-    int *state = malloc(numsSize * sizeof(int));
-    bool *selected = calloc(numsSize, sizeof(bool));
+    int** ans = malloc(sizeof(int*) * ansSize);
+    int* colans = malloc(sizeof(int) * ansSize);
+
+    int* state = malloc(sizeof(int) * numsSize);
+    bool* select = calloc(numsSize, sizeof(bool));
 
     void dfs(int state_size) {
         if (state_size == numsSize) {
-            int *tmp = malloc(sizeof(int) * numsSize);
+            int* tmp = malloc(sizeof(int) * numsSize);
             memcpy(tmp, state, sizeof(int) * numsSize);
             ans[*returnSize] = tmp;
             colans[(*returnSize)++] = state_size;
             return;
         }
+        bool hash[21] = {false};
         for (int i = 0; i < numsSize; i++) {
-            if (selected[i]) continue;
-            selected[i] = true;
+            if (select[i] || hash[nums[i] + 10]) continue;
+            hash[nums[i] + 10] = true;
+            select[i] = true;
             state[state_size] = nums[i];
+
             dfs(state_size + 1);
-            selected[i] = false;
+
+            select[i] = false;
         }
     }
 
     dfs(0);
-
-    free(state);
-    free(selected);
 
     *returnColumnSizes = colans;
     return ans;
--- exercise/Backtracking/ex47/method2.c
+++ exercise/Backtracking/ex47/method2.c
@@ -3,40 +3,47 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int** permute(int *nums, int numsSize, int *returnSize, int **returnColumnSizes) {
+
+int compare(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+int** permuteUnique(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
     *returnSize = 0;
 
     int ansSize = 1;
-    for (int i = 2; i <= numsSize; i++) {
+    for (int i = 1; i <= numsSize; i++) {
         ansSize *= i;
     }
-    int **ans = malloc(sizeof(int *) * ansSize);
-    int *colans = malloc(sizeof(int) * ansSize);
 
-    int *state = malloc(numsSize * sizeof(int));
-    bool *selected = calloc(numsSize, sizeof(bool));
+    int** ans = malloc(sizeof(int*) * ansSize);
+    int* colans = malloc(sizeof(int) * ansSize);
+
+    int* state = malloc(sizeof(int) * numsSize);
+    bool* select = calloc(numsSize, sizeof(bool));
 
     void dfs(int state_size) {
         if (state_size == numsSize) {
-            int *tmp = malloc(sizeof(int) * numsSize);
+            int* tmp = malloc(sizeof(int) * numsSize);
             memcpy(tmp, state, sizeof(int) * numsSize);
             ans[*returnSize] = tmp;
             colans[(*returnSize)++] = state_size;
             return;
         }
         for (int i = 0; i < numsSize; i++) {
-            if (selected[i]) continue;
-            selected[i] = true;
+            if (select[i] || (i > 0 && nums[i] == nums[i - 1]) && !select[i - 1]) continue;
+            select[i] = true;
             state[state_size] = nums[i];
+
             dfs(state_size + 1);
-            selected[i] = false;
+
+            select[i] = false;
         }
     }
 
     dfs(0);
-
-    free(state);
-    free(selected);
 
     *returnColumnSizes = colans;
     return ans;
